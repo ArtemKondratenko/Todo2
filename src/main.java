41fce9e7b07b159b63742457f242a147f3c3bc3a
Артем Кондратенко <artem-osteo@yandex.ru>
2022-12-14 14:47:09 +0300

    Add

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int ticket_price = 1000;
+        int rubles_mile = 20;
+        int num_miles = ticket_price / rubles_mile;
+
+        System.out.println(num_miles );
+    }
+}
