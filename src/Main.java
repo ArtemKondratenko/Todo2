2763821bdf49c91bc7ec6ca340ec36bcffcaae92
ArtemKondratenko <100959753+ArtemKondratenko@users.noreply.github.com>
2022-12-18 18:13:11 +0300

    Update and rename main.java to Main.java
    Correction

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1000;
+        int rublesMile = 20;
+        int numMiles = ticketPrice / rublesMile;
+
+        System.out.println(numMiles);
+    }
+}
--- src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        int ticket_price = 1000;
-        int rubles_mile = 20;
-        int num_miles = ticket_price / rubles_mile;
-
-        System.out.println(num_miles );
-    }
-}
